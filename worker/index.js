2931335c57895c3f572f7b57d2773b0af347fb7a
Overdrive Accounting Services <Info@OverdriveAccountingServices.com>
2026-09-21 18:43:00 -0400

    Use supported PBKDF2 iteration count

--- worker/index.js
+++ worker/index.js
@@ -58,7 +58,7 @@ function hexToBytes(value) {
 
 async function hashPassword(password, salt = crypto.getRandomValues(new Uint8Array(16))) {
   const key = await crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'PBKDF2', false, ['deriveBits'])
-  const bits = await crypto.subtle.deriveBits({ name: 'PBKDF2', salt, iterations: 120000, hash: 'SHA-256' }, key, 256)
+  const bits = await crypto.subtle.deriveBits({ name: 'PBKDF2', salt, iterations: 100000, hash: 'SHA-256' }, key, 256)
   return `${bytesToHex(salt)}:${bytesToHex(bits)}`
 }
 
